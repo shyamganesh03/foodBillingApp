c4cc887c52c14564bd03c24cb761e5f02a2a0f88
shyam ganesh ravichandran <shyamganesh@shyams-MacBook-Air.local>
2023-07-19 18:29:20 +0530

    fix(ios): android and ios issues fixed

--- packages/shared/src/App.js
+++ packages/shared/src/App.js
@@ -9,31 +9,33 @@ import {
 } from 'react-native-safe-area-context'
 import { View, useWindowDimensions } from 'react-native'
 import awsConfig from '../awsConfig'
+import DashBoard from './screens/dashboard'
 
 const App = () => {
   const routeNameRef = useRef()
   Amplify.configure(awsConfig)
   const height = useWindowDimensions().height
 
-  return (
-    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-      <NavigationContainer
-        ref={navigationRef}
-        onReady={() =>
-          (routeNameRef.current = navigationRef.current.getCurrentRoute().name)
-        }
-        onStateChange={async () => {
-          const currentRouteName = navigationRef.current.getCurrentRoute().name
-          routeNameRef.current = currentRouteName
-        }}
-        linking={{ enabled: true }}
-      >
-        <View style={{ minHeight: height }}>
-          <AppNavigator />
-        </View>
-      </NavigationContainer>
-    </SafeAreaProvider>
-  )
+  // return (
+  //   <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+  //     <NavigationContainer
+  //       ref={navigationRef}
+  //       onReady={() =>
+  //         (routeNameRef.current = navigationRef.current.getCurrentRoute().name)
+  //       }
+  //       onStateChange={async () => {
+  //         const currentRouteName = navigationRef.current.getCurrentRoute().name
+  //         routeNameRef.current = currentRouteName
+  //       }}
+  //       linking={{ enabled: true }}
+  //     >
+  //       <View style={{ minHeight: height }}>
+  //         <AppNavigator />
+  //       </View>
+  //     </NavigationContainer>
+  //   </SafeAreaProvider>
+  // )
+  return <DashBoard />
 }
 
 export default App
--- packages/shared/src/screens/dashboard/MobileView.js
+++ packages/shared/src/screens/dashboard/MobileView.js
@@ -1,5 +1,4 @@
-import { View } from 'react-native'
-import { Text } from '@libs/components'
+import { View, Text } from 'react-native'
 import React from 'react'
 
 const MobileView = () => {
