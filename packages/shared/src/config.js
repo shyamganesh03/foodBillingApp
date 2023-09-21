06f2f8a83cd2ed0e5d193ba637c4b3e3288b543b
yokesh.ks <yokesh@increscotech.com>
2023-09-21 22:24:17 +0530

    fix(url): clous front url changed from s3

--- packages/shared/src/config.js
+++ packages/shared/src/config.js
@@ -1,5 +1,4 @@
 export const sentryUrl =
   'https://277b4676e9f448ab9508ec958cbb970c@o4504434079301632.ingest.sentry.io/4504434081660928'
 
-export const r3AppUrl =
-  'http://r3-oaf-frontend-dev.s3-website-eu-west-1.amazonaws.com'
+export const r3AppUrl = 'https://d2kcomqj04getx.cloudfront.net'
