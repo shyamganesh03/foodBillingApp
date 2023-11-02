aa4b2585cb1580bb52a1f32c71f138a8eb32cee9
shyam <shyamganesh@increscotech.com>
2023-11-02 23:20:19 +0530

    feat(firebase): firebase initialized

--- apps/mobile/ios/foodbillingappTests/foodbillingappTests.m
+++ apps/mobile/ios/foodbillingappTests/foodbillingappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface increscoTests : XCTestCase
+@interface foodbillingappTests : XCTestCase
 
 @end
 
-@implementation increscoTests
+@implementation foodbillingappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
